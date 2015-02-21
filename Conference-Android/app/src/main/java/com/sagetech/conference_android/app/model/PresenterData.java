a2ec526a5b11725610076be7bc69e93c945c18fc
Carlus Henry <carlushenry@gmail.com>
2015-02-20 19:39:38 -0500

    adding presenter transformation for the api that is there...

--- Conference-Android/app/src/main/java/com/sagetech/conference_android/app/model/PresenterData.java
+++ Conference-Android/app/src/main/java/com/sagetech/conference_android/app/model/PresenterData.java
@@ -4,4 +4,41 @@ package com.sagetech.conference_android.app.model;
  * Created by carlushenry on 2/19/15.
  */
 public class PresenterData {
+
+    private Long id;
+    private String userId;
+    private String shortBio;
+    private String jobTitle;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserId() {
+        return userId;
+    }
+
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public String getShortBio() {
+        return shortBio;
+    }
+
+    public void setShortBio(String shortBio) {
+        this.shortBio = shortBio;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
 }
--- Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/presenter/EventDetailViewBuilder.java
+++ Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/presenter/EventDetailViewBuilder.java
@@ -24,11 +24,33 @@ public class EventDetailViewBuilder {
         dto.setStartDttm(eventData.getStartDttm());
         dto.setEndDttm(addMinutes(dto.getStartDttm(), eventData.getDurationMinutes()));
         dto.setTitle(eventData.getName());
-        dto.setPresenters(null); // TODO
+        dto.setPresenters(toPresenters(presenterDataList)); // TODO
         dto.setEventType(null); // TODO
         dto.setRoomName(null); // TODO
 
         return dto;
+    }
+
+    private List<EventDetailView.EventDetailPresenterView> toPresenters(List<PresenterData> presenterDataList) {
+
+        List<EventDetailView.EventDetailPresenterView> presenterList = new ArrayList<EventDetailView.EventDetailPresenterView>();
+
+        for (PresenterData currPresenter : presenterDataList) {
+            presenterList.add(toPresenter(currPresenter));
+        }
+
+
+        return presenterList;
+    }
+
+    private EventDetailView.EventDetailPresenterView toPresenter(PresenterData currPresenter) {
+        EventDetailView.EventDetailPresenterView presenter = new EventDetailView().new EventDetailPresenterView();
+
+        presenter.setFirstName(currPresenter.getId() + "");
+        presenter.setLastName(currPresenter.getUserId());
+        presenter.setBiography(currPresenter.getShortBio());
+
+        return presenter;
     }
 
     private Date addMinutes(Date date, Integer durationMinutes) {
@@ -36,44 +58,6 @@ public class EventDetailViewBuilder {
         cal.setTime(date);
         cal.add(Calendar.MINUTE, durationMinutes);
         return cal.getTime();
-    }
-
-    private List<EventDetailView.EventDetailPresenterView> toPresenters(EventData eventData) {
-
-        List<EventDetailView.EventDetailPresenterView> presenterList = new ArrayList<EventDetailView.EventDetailPresenterView>();
-
-        List<EventData.Event_leader> eventLeaders = eventData.getEvent_leaders();
-
-        for (EventData.Event_leader currEventLeader : eventLeaders) {
-            presenterList.add(toPresenter(currEventLeader));
-        }
-
-
-        return presenterList;
-    }
-
-    private EventDetailView.EventDetailPresenterView toPresenter(EventData.Event_leader currEventLeader) {
-        EventDetailView.EventDetailPresenterView presenter = new EventDetailView().new EventDetailPresenterView();
-
-        presenter.setFirstName(currEventLeader.getFirst_name());
-        presenter.setLastName(currEventLeader.getLast_name());
-        presenter.setBiography(currEventLeader.getBiography());
-
-        return presenter;
-    }
-
-    private Date toDate(String dateString) {
-        return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS").parse(dateString, new ParsePosition(0));
-    }
-
-    private EventDetailView.EventType toEventType(EventData.Event event) {
-        EventDetailView.EventType eventType = null;
-        if ("Presentation".equals(event.getEvent_type())) {
-            eventType = EventDetailView.EventType.PRESENTATION;
-        } else if ("Code Lab".equals(event.getEvent_type())) {
-            eventType = EventDetailView.EventType.CODE_LABS;
-        }
-        return eventType;
     }
 
 }
