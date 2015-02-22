778a1bb62a33914ff9953055a56638682c6cd840
jonas <jonas@geekyrooster.com>
2015-02-21 22:57:11 -0500

    wired conference sessions to event detail

--- Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/activities/ConferenceListActivity.java
+++ Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/activities/ConferenceListActivity.java
@@ -5,21 +5,19 @@ import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.sagetech.conference_android.app.R;
 import com.sagetech.conference_android.app.api.ConferenceController;
-import com.sagetech.conference_android.app.model.ConferenceSessionData;
-import com.sagetech.conference_android.app.ui.listener.RecyclerItemClickListener;
-import com.sagetech.conference_android.app.ui.presenter.ConferenceSessionListActivityPresenter;
-import com.sagetech.conference_android.app.ui.presenter.IConferenceSessionActivity;
 import com.sagetech.conference_android.app.model.ConferenceData;
 import com.sagetech.conference_android.app.ui.listener.RecyclerItemClickListener;
 import com.sagetech.conference_android.app.ui.presenter.ConferenceListActivityPresenter;
 import com.sagetech.conference_android.app.ui.presenter.IConferenceListActivity;
-import timber.log.Timber;
+
 import java.util.List;
 
 import javax.inject.Inject;
@@ -28,28 +26,18 @@ import butterknife.ButterKnife;
 import butterknife.InjectView;
 import timber.log.Timber;
 
-/**
- * Created by adam on 2/21/15.
- */
-public class ConferenceSessionListActivity extends InjectableActionBarActivity implements IConferenceSessionActivity {
+public class ConferenceListActivity extends InjectableActionBarActivity implements IConferenceListActivity {
 
     @Inject
     ConferenceController conferenceController;
-
 
     @InjectView(R.id.confView)
     RecyclerView mRecyclerView;
 
     private RecyclerView.Adapter mAdapter;
     private RecyclerView.LayoutManager mLayoutManager;
-    private ConferenceSessionListActivityPresenter presenter = null;
-    private Integer conferenceId;
 
-    @Override
-    public void populateConferenceSessions(List<ConferenceSessionData> datas) {
-        mAdapter = new ConferenceSessionsAdapter(datas);
-        mRecyclerView.setAdapter(mAdapter);
-    }
+    private ConferenceListActivityPresenter presenter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -69,42 +57,68 @@ public class ConferenceSessionListActivity extends InjectableActionBarActivity i
 
                         Timber.d(String.format("View Clicked: %s", view.getTag()));
 
-                        Intent eventDetailIntent = new Intent(view.getContext(), EventDetailActivity.class);
-                        eventDetailIntent.putExtra("id", (Long)view.getTag());
-                        startActivity(eventDetailIntent);
+                        Intent conferenceSessionListIntent = new Intent(view.getContext(), ConferenceSessionListActivity.class);
+                        conferenceSessionListIntent.putExtra("id", (Integer) view.getTag());
+                        startActivity(conferenceSessionListIntent);
                     }
                 })
         );
 
-        conferenceId = getIntent().getExtras().getInt("id");
-
-        presenter = new ConferenceSessionListActivityPresenter(this, conferenceController, Long.valueOf(conferenceId));
+        presenter = new ConferenceListActivityPresenter(conferenceController, this);
         presenter.initialize();
     }
 
 
-    public class ConferenceSessionsAdapter extends RecyclerView.Adapter<ConferenceSessionsAdapter.ViewHolder> {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_conferences, menu);
+        return true;
+    }
 
-        List<ConferenceSessionData> conferenceSessionDatas;
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void populateConferences(List<ConferenceData> datas) {
+        mAdapter = new ConferencesAdapter(datas);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+
+    public class ConferencesAdapter extends RecyclerView.Adapter<ConferencesAdapter.ViewHolder> {
+
+        List<ConferenceData> conferenceDatas;
 
         public class ViewHolder extends RecyclerView.ViewHolder {
             // each data item is just a string in this case
             public View mTextView;
-
             public ViewHolder(View v) {
                 super(v);
                 mTextView = v;
             }
         }
 
-        public ConferenceSessionsAdapter(List<ConferenceSessionData> conferenceSessionDatas) {
-            this.conferenceSessionDatas = conferenceSessionDatas;
+        public ConferencesAdapter(List<ConferenceData> conferenceDatas) {
+            this.conferenceDatas = conferenceDatas;
         }
 
         @Override
-        public ConferenceSessionsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        public ConferencesAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
             View v = LayoutInflater.from(parent.getContext())
-                    .inflate(R.layout.conference_sesssion_list_view, parent, false);
+                    .inflate(R.layout.conference_list_view_item, parent, false);
 
             ViewHolder vh = new ViewHolder(v);
 
@@ -112,15 +126,15 @@ public class ConferenceSessionListActivity extends InjectableActionBarActivity i
         }
 
         @Override
-        public void onBindViewHolder(ConferenceSessionsAdapter.ViewHolder holder, int position) {
-            TextView txt = (TextView) holder.itemView.findViewById(R.id.name);
-            holder.itemView.setTag(conferenceSessionDatas.get(position).getId());
-            txt.setText(conferenceSessionDatas.get(position).getName());
+        public void onBindViewHolder(ConferencesAdapter.ViewHolder holder, int position) {
+            TextView txt = (TextView)holder.itemView.findViewById(R.id.name);
+            holder.itemView.setTag(conferenceDatas.get(position).getId());
+            txt.setText(conferenceDatas.get(position).getName());
         }
 
         @Override
         public int getItemCount() {
-            return conferenceSessionDatas.size();
+            return conferenceDatas.size();
         }
 
     }
