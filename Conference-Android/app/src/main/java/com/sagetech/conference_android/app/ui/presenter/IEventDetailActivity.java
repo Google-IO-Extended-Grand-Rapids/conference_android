c78792248ca39c2045fbc2deb02c5a198f3f37d6
Carlus Henry <carlushenry@gmail.com>
2015-03-07 08:56:25 -0500

    renaming classes to viewmodel...

--- Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/presenter/IEventDetailActivity.java
+++ Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/presenter/IEventDetailActivity.java
@@ -1,11 +1,11 @@
 package com.sagetech.conference_android.app.ui.presenter;
 
-import com.sagetech.conference_android.app.ui.viewModel.EventDetailView;
+import com.sagetech.conference_android.app.ui.viewModel.EventDetailViewModel;
 
 /**
  * Created by carlushenry on 2/19/15.
  */
 public interface IEventDetailActivity {
 
-    public void populateWithEventDetailView(EventDetailView eventDetailView);
+    public void populateWithEventDetailView(EventDetailViewModel eventDetailViewModel);
 }
