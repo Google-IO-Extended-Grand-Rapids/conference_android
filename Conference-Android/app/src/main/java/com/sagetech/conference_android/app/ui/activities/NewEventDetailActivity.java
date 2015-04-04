f001213455f9c49ee759e75f27743634a74cb0af
jonas <jonas@geekyrooster.com>
2015-04-04 07:45:23 -0400

    stubbing in views into EventDetail

--- Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/activities/NewEventDetailActivity.java
+++ Conference-Android/app/src/main/java/com/sagetech/conference_android/app/ui/activities/NewEventDetailActivity.java
@@ -2,16 +2,51 @@ package com.sagetech.conference_android.app.ui.activities;
 
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.sagetech.conference_android.app.R;
+import com.sagetech.conference_android.app.ui.presenter.EventDetailActivityPresenter;
+import com.sagetech.conference_android.app.ui.presenter.IEventDetailActivity;
+import com.sagetech.conference_android.app.ui.viewModel.EventDetailViewModel;
 
-import butterknife.ButterKnife;
+import javax.inject.Inject;
+
+import butterknife.InjectView;
 import timber.log.Timber;
 
 /**
  * Created by carlushenry on 3/15/15.
  */
-public class NewEventDetailActivity extends ActionBarActivity {
+public class NewEventDetailActivity extends ActionBarActivity implements IEventDetailActivity {
+
+    @Inject
+    EventDetailActivityPresenter presenter;
+
+    @InjectView(R.id.txtTitle)
+    TextView title;
+
+    @InjectView(R.id.txtSchedule)
+    TextView schedule;
+
+    @InjectView(R.id.txtRoom)
+    TextView room;
+
+    @InjectView(R.id.imgSessionType)
+    ImageView sessionType;
+
+    @InjectView(R.id.txtDescription)
+    TextView description;
+
+    @InjectView(R.id.imgPin)
+    ImageView pin;
+
+    @InjectView(R.id.txtRecommend)
+    TextView recommend;
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,10 +57,18 @@ public class NewEventDetailActivity extends ActionBarActivity {
 
         Timber.d("onCreate");
 
+        Long eventId = getIntent().getExtras().getLong("id");
+        presenter.initialize(eventId);
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
+    }
+
+    @Override
+    public void populateWithEventDetailView(EventDetailViewModel eventDetailViewModel) {
+
+
     }
 }
